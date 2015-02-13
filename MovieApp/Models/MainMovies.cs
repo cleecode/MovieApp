1a61332c524caa65c2281a6ca87a467b3b5bdce5
itsaclee <clee86@gmail.com>
2015-02-12 21:19:56 -0800

    first commit

---
+++ MovieApp/Models/MainMovies.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace MovieApp.Models
+{
+    public class MainMovies
+    {
+
+        //Movie m = JsonConvert.DeserializeObject<Movie>(json);
+
+
+
+        //Movie m = JsonConvert.DeserializeObject<Movie>(json);
+
+        //string name = m.Name;
+        // Bad Boys
+        
+        
+        
+        
+        //private static string RottenTomatoesKey = "hjrfd5tmbh6qaa58ebb952yb";
+        
+        //static void Main(string[] args)
+        //{
+        //    try
+        //    {
+        //        string movieRequest = CreateRequest()
+        //    }
+        //    catch (Exception)
+        //    {
+                
+        //        throw;
+        //    }
+        //}
+
+ 
+    }
+}
